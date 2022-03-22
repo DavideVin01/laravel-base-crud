32ccff6236459e504f569465cb96f6e17a3444e6
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-22 19:42:20 +0100

    Creates cards

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,5 +2,36 @@
 @section('page-title', 'Comics')
 
 @section('title')
-    <h1>Comics</h1>
+    <div class="container mt-3">
+        <h1>COMICS</h1>
+    </div>
+@endsection
+
+@section('content')
+    <div class="container">
+        <h3>Elenco di tutti i fumetti <strong>DC COMICS</strong></h3>
+    </div>
+
+    <div class="d-flex flex-wrap container my-3">
+        @foreach ($comics as $comic)
+            <div class="card" style="width: 18rem;">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
